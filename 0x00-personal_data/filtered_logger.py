da0236326716f731d37945d25a70fbf5c5176574
Sobilo34 <bilalsolih60@gmail.com>
2024-08-01 02:50:52 +0100

    uncomment the task 2 code

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -17,18 +17,19 @@ def filter_datum(fields: List[str], redaction: str,
                          f"{field}={redaction}{separator}", message)
     return message
 
-# class RedactingFormatter(logging.Formatter):
-#     """ Redacting Formatter class
-#         """
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
 
-#     REDACTION = "***"
-#     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-#     SEPARATOR = ";"
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
 
-#     def __init__(self, fields: List[str]):
-#         super(RedactingFormatter, self).__init__(self.FORMAT)
-#         self.fields = fields
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
 
-#     def format(self, record: logging.LogRecord) -> str:
-#         record.msg = filter_datum(self.fields, self.REDACTION, record.msg, self.SEPARATOR)
-#         return super().format(record)
+    def format(self, record: logging.LogRecord) -> str:
+        record.msg = filter_datum(
+            self.fields, self.REDACTION, record.msg, self.SEPARATOR)
+        return super().format(record)
