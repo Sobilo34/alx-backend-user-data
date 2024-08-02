cb7bd7ef3c45d52d90d4bcbcdf986f2b765c3258
Sobilo34 <bilalsolih60@gmail.com>
2024-08-02 05:43:45 +0100

    added mysql db function

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,6 +5,8 @@ A function that returns the log message obfuscated:
 import re
 import logging
 from typing import List
+import os
+import mysql.connector # type: ignore
 
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
@@ -52,3 +54,48 @@ def get_logger() -> logging.Logger:
     logger.propagate = False
     logger.addHandler(stream_handler)
     return logger
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """
+    The database function
+    """
+    host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
+    database = os.getenv("PERSONAL_DATA_DB_NAME", "")
+    user = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
+    password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
+    connection = mysql.connector.connect(
+        host=host, user=user,
+        port=3306,
+        password=password, database=database
+    )
+    return connection
+
+
+
+def main():
+    """
+    Main function to retrieve data from the database,
+    obfuscate sensitive fields, and log it.
+    """
+    fields = "name,email,phone,ssn,password,ip,last_login,user_agent"
+    columns = fields.split(',')
+    query = "SELECT {} FROM users;".format(fields)
+    info_logger = get_logger()
+    connection = get_db()
+    with connection.cursor() as cursor:
+        cursor.execute(query)
+        rows = cursor.fetchall()
+        for row in rows:
+            record = map(
+                lambda x: '{}={}'.format(x[0], x[1]),
+                zip(columns, row),
+            )
+            message = '{};'.format('; '.join(list(record)))
+            args = ("user_data", logging.INFO, None, None, message, None, None)
+            log_record = logging.LogRecord(*args)
+            info_logger.handle(log_record)
+
+
+if __name__ == "__main__":
+    main()
