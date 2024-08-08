29feb83440931f1b03a2245cd56367cdb939d5af
Sobilo34 <bilalsolih60@gmail.com>
2024-08-08 10:57:50 +0100

    created the Auth class

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""A create a class to manage the API authentication"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """A class to manage the API authentication"""
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """A method that require authentication"""
+        if path is None or excluded_paths is None or excluded_paths == []:
+            return True
+        if path[-1] != '/':
+            path += '/'
+        if path in excluded_paths:
+            return False
+        return True
+
+
+    def authorization_header(self, request=None) -> str:
+        """A method that return the value of the header request"""
+        if request is None or 'Authorization' not in request.headers:
+            return None
+        return request.headers['Authorization']
+
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """A method that return None"""
+        return None
