c69c621f1c375e03e41d85d6bdeccfae1a3e1a52
Sobilo34 <bilalsolih60@gmail.com>
2024-08-10 21:55:39 +0100

    create method for deleteing a user and logging out

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -50,3 +50,14 @@ def login():
     response.set_cookie(session_name, session_id)
 
     return response
+
+@app_views.route('/auth_session/logout', methods=['DELETE'],
+                 strict_slashes=False)
+def handle_logout():
+    """
+    Delete user and logout
+    """
+    from api.v1.app import auth
+    if auth.destroy_session(request):
+        return jsonify({}), 200
+    abort(404)
