7c8ddee0ff73afd8fecdbe64cc4018fc105b8740
Sobilo34 <bilalsolih60@gmail.com>
2024-08-10 22:28:26 +0100

    initializez app.py

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -10,6 +10,8 @@ import os
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
 from api.v1.auth.session_auth import SessionAuth
+from api.v1.auth.session_exp_auth import SessionExpAuth
+from api.v1.auth.session_db_auth import SessionDBAuth
 from models.user import User
 
 app = Flask(__name__)
@@ -21,68 +23,68 @@ auth = None
 # We load and assign the right instance of authentication to auth
 AUTH_TYPE = getenv("AUTH_TYPE")
 
-# add auth for if AUTH_TYPE is auth
 if AUTH_TYPE == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
-
-# add basic auth for if AUTH_TYPE is basic_auth
-if AUTH_TYPE == "basic_auth":
+elif AUTH_TYPE == "basic_auth":
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
-
-# add session auth for if AUTH_TYPE is session_auth
-if AUTH_TYPE == "session_auth":
+elif AUTH_TYPE == "session_auth":
     from api.v1.auth.session_auth import SessionAuth
     auth = SessionAuth()
-
-# add session exp auth for if AUTH_TYPE is session_exp_auth
-if AUTH_TYPE == "session_exp_auth":
+elif AUTH_TYPE == "session_exp_auth":
     from api.v1.auth.session_exp_auth import SessionExpAuth
     auth = SessionExpAuth()
+elif AUTH_TYPE == "session_db_auth":
+    from api.v1.auth.session_db_auth import SessionDBAuth
+    auth = SessionDBAuth()
 
 
 @app.errorhandler(404)
 def not_found(error) -> str:
-    """ Not found handler
-    """
+    """ Not found handler """
     return jsonify({"error": "Not found"}), 404
 
 
 @app.errorhandler(401)
 def unauthorized(error) -> str:
-    """ Unauthorized handler
-    """
+    """ Unauthorized handler """
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
 def forbidden(error) -> str:
-    """ Forbidden handler
-    """
+    """ Forbidden handler """
     return jsonify({"error": "Forbidden"}), 403
 
 
 @app.before_request
-def before_request() -> str:
-    """ Handler before each request to validate authentication.
-    """
+def before_request() -> None:
+    """ Before request handler to validate authentication """
     if auth is None:
         return
-    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/', '/api/v1/auth_session/login/']
+
+    excluded_paths = [
+        '/api/v1/status/',
+        '/api/v1/unauthorized/',
+        '/api/v1/forbidden/',
+        '/api/v1/auth_session/login/'
+    ]
+
     if not auth.require_auth(request.path, excluded_paths):
         return
-    if auth.authorization_header(request) is None and auth.session_cookie(
-                    request) is None:
+
+    if auth.authorization_header(request) is None and auth.session_cookie(request) is None:
         abort(401)
+
     current_user = auth.current_user(request)
     if current_user is None:
         abort(403)
+
     request.current_user = current_user
 
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
-    app.run(debug=True, host=host, port=port)
+    app.run(host=host, port=port)
