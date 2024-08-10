4e43dd1ae65ec4710477d6eecd86e2ad698ac300
Sobilo34 <bilalsolih60@gmail.com>
2024-08-10 18:12:54 +0100

    Update SessionAuth class with more featires

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -6,12 +6,23 @@ from typing import Tuple, TypeVar
 from base64 import b64decode
 from models.user import User
 from flask import request
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     """A session authentication class"""
+    user_id_by_session_id = {}
+
     def __init__(self):
         """Initialize the SessionAuth class"""
         super().__init__()
 
-    # Add your implementation here
+    def create_session(self, user_id: str = None) -> str:
+        """
+        An instance method that creates a Session ID for a user_id
+        """
+        if user_id is None or type(user_id) is not str:
+            return None
+        session_id = str(uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
