39c5c9a9989d5eff9f06299ad57aba79e6fdf494
Sobilo34 <bilalsolih60@gmail.com>
2024-08-10 18:31:08 +0100

    added user_id_for_session_id) for storing and retrieving a link between a User ID and a Session ID.

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -26,3 +26,13 @@ class SessionAuth(Auth):
         session_id = str(uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """
+        An instance method that returns a User ID based on a Session ID
+        """
+        if session_id is None or not isinstance(session_id, str):
+            return None
+
+        user_id = self.user_id_by_session_id.get(session_id)
+        return user_id
