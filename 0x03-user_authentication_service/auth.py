c99ce93a327994a0acc342affb89496456aae89a
Sobilo34 <bilalsolih60@gmail.com>
2024-08-14 23:27:38 +0100

    hashed a password and return it hashed byte using bcrypt

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python3
+"""
+Authentication module
+"""
+import bcrypt
+
+def _hash_password(password: str) -> bytes:
+    """
+    returned bytes is a salted hash of the input password
+    """
+    bytes = password.encode('utf-8')
+    salt = bcrypt.gensalt()
+    hash = bcrypt.hashpw(bytes, salt)
+    return hash
