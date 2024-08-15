dc5bd9d7f490f2c0327a7035558ed081458133b4
Sobilo34 <bilalsolih60@gmail.com>
2024-08-15 01:44:23 +0100

    resolve pycodestyle error

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -17,6 +17,7 @@ def my_form():
     """
     return jsonify({"message": "Bienvenue"})
 
+
 @app.route('/users', methods=['POST'], strict_slashes=False)
 def register_user() -> str:
     """Register a user
@@ -28,6 +29,7 @@ def register_user() -> str:
         return jsonify(email=user.email, message='user created'), 200
     except ValueError:
         return jsonify(message="email already registered"), 400
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
