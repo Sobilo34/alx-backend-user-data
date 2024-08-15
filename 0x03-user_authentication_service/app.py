327bdfcb1981dbfc545eecadf1edd73bdda8a13e
Sobilo34 <bilalsolih60@gmail.com>
2024-08-15 01:42:54 +0100

    In this task, you will implement the end-point to register a user. Define a users function that implements the POST /users route.
    Import the Auth object and instantiate it at the root of the module as such:
    
    from auth import Auth
    
    AUTH = Auth()
    The end-point should expect two form data fields

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,10 +2,12 @@
 """
 The flask app
 """
-from flask import Flask
+from flask import Flask, request
 from flask import jsonify
+from auth import Auth
 
 
+AUTH = Auth()
 app = Flask(__name__)
 
 
@@ -15,6 +17,17 @@ def my_form():
     """
     return jsonify({"message": "Bienvenue"})
 
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def register_user() -> str:
+    """Register a user
+    """
+    try:
+        email = request.form['email']
+        password = request.form['password']
+        user = AUTH.register_user(email, password)
+        return jsonify(email=user.email, message='user created'), 200
+    except ValueError:
+        return jsonify(message="email already registered"), 400
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
